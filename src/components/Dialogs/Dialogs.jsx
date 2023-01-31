14438672e9e6657f6ddd4425191db20b4a399da9
Bohdan Stefankiv <stefankivj@gmail.com>
2023-01-31 17:21:09 +0200

    dialogs modifying less21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import cl from "./Dialogs.module.css";
+let love = "<3";
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={cl.dialogs}>
+      <div className={cl.dialogs_items}>
+        <div className={cl.dialog}>Mamba</div>
+        <div className={cl.dialog}>Jessy</div>
+        <div className={cl.dialog}>Ally</div>
+        <div className={cl.dialog}>Delpan</div>
+        <div className={cl.dialog}>Jane</div>
+      </div>
+      <div className={cl.messages}>
+        <div className={cl.message}>Hi!</div>
+        <div className={cl.message}>How are you?</div>
+        <div className={cl.message}>Are you here?</div>
+        <div className={cl.message}>hahahhaa</div>
+        <div className={cl.message}>{love}</div>
+      </div>
+    </div>
+  );
 };
 export default Dialogs;
