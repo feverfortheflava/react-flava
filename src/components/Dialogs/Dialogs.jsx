d0b52cf1fa05961919baf63642c11ed9747e915e
Bohdan Stefankiv <stefankivj@gmail.com>
2023-02-02 16:16:20 +0200

    arrays prep for Map, less247

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,21 +6,35 @@ import Message from "./Message/Message";
 let love = "<3";
 
 const Dialogs = () => {
+  let dialogsData = [
+    { id: 1, name: "Mamba" },
+    { id: 2, name: "Jessy" },
+    { id: 3, name: "Ally" },
+    { id: 4, name: "Delpan" },
+    { id: 5, name: "Jane" },
+  ];
+  let messagesData = [
+    { id: 1, message: "Hi!" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Are you here?" },
+    { id: 4, message: "hahahahahah" },
+    { id: 5, message: love },
+  ];
   return (
     <div className={cl.dialogs}>
       <div className={cl.dialogs_items}>
-        <DialogItem name="Mamba" id="1" />
-        <DialogItem name="Jessy" id="2" />
-        <DialogItem name="Ally" id="3" />
-        <DialogItem name="Delpan" id="4" />
-        <DialogItem name="Jane" id="5" />
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
       </div>
       <div className={cl.messages}>
-        <Message message="Hi!" />
-        <Message message="How are you?" />
-        <Message message="Are you here?" />
-        <Message message="hahahahahah" />
-        <Message message={love} />
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        <Message message={messagesData[2].message} />
+        <Message message={messagesData[3].message} />
+        <Message message={messagesData[4].message} />
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,19 @@ import React from "react"; //паше і без цього
 import cl from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = () => {
+  let postData = [
+    { id: 1, message: "hi guys", likes: 19 },
+    { id: 2, message: "hi friends", likes: 22 },
+    { id: 3, message: "<3", likes: 32 },
+    { id: 4, message: "hahahahahah", likes: 5 },
+    { id: 5, message: "", likes: 5 },
+  ];
   return (
     <div>
       <div>My posts</div>
       <div className={cl.posts}>
-        <Post message="hi guys" likes="16" />
-        <Post message="hi freinds" likes="25" />
+        <Post message={postData[0].message} likes={postData[0].likes} />
+        <Post message={postData[1].message} likes={postData[1].likes} />
         <Post />
         <Post />
         <Post />
