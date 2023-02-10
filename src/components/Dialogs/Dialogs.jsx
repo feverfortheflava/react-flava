ac5a12b3852e904876c2d637e88adb0ea3478660
Bohdan Stefankiv <stefankivj@gmail.com>
2023-02-10 18:51:48 +0200

    [].map() less25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,36 +6,28 @@ import Message from "./Message/Message";
 let love = "<3";
 
 const Dialogs = () => {
-  let dialogsData = [
+  let dialogs = [
     { id: 1, name: "Mamba" },
     { id: 2, name: "Jessy" },
     { id: 3, name: "Ally" },
     { id: 4, name: "Delpan" },
     { id: 5, name: "Jane" },
   ];
-  let messagesData = [
+  let messages = [
     { id: 1, message: "Hi!" },
     { id: 2, message: "How are you?" },
     { id: 3, message: "Are you here?" },
     { id: 4, message: "hahahahahah" },
     { id: 5, message: love },
   ];
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+  let messagesElements = messages.map((m) => <Message message={m.message} />);
   return (
     <div className={cl.dialogs}>
-      <div className={cl.dialogs_items}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-      </div>
-      <div className={cl.messages}>
-        <Message message={messagesData[0].message} />
-        <Message message={messagesData[1].message} />
-        <Message message={messagesData[2].message} />
-        <Message message={messagesData[3].message} />
-        <Message message={messagesData[4].message} />
-      </div>
+      <div className={cl.dialogs_items}>{dialogsElements}</div>
+      <div className={cl.messages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,23 +2,20 @@ import React from "react"; //паше і без цього
 import cl from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = () => {
-  let postData = [
+  let posts = [
     { id: 1, message: "hi guys", likes: 19 },
     { id: 2, message: "hi friends", likes: 22 },
     { id: 3, message: "<3", likes: 32 },
     { id: 4, message: "hahahahahah", likes: 5 },
-    { id: 5, message: "", likes: 5 },
+    { id: 5, message: "lol", likes: 5 },
   ];
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likes={p.likes} />
+  ));
   return (
     <div>
       <div>My posts</div>
-      <div className={cl.posts}>
-        <Post message={postData[0].message} likes={postData[0].likes} />
-        <Post message={postData[1].message} likes={postData[1].likes} />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+      <div className={cl.posts}>{postsElements}</div>
     </div>
   );
 };
